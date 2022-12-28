930a5aa8992b82b2db171362e9cbbc494a89dc7d
Kevin.yu <xuapy5941@gmail.com>
2022-12-28 13:22:05 +0800

    feat: add chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppType, AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
 import { DefaultLayout } from '~/components/DefaultLayout';
 import { trpc } from '~/utils/trpc';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export type NextPageWithLayout<
   TProps = Record<string, unknown>,
@@ -19,7 +20,11 @@ const MyApp = (({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout =
     Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <ChakraProvider>
+      <Component {...pageProps} />{' '}
+    </ChakraProvider>,
+  );
 }) as AppType;
 
 export default trpc.withTRPC(MyApp);
